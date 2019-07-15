0452562ffbe1a5230320e54bcadae7ecbb6b7c31
maggie-bao202 <maggie.bao202@gmail.com>
2019-07-15 11:04:55 -0700

    Add files via upload

---
+++ Practice.java
@@ -0,0 +1,16 @@
+
+public class Practice {
+	public Practice() {
+		int integer = 8;
+		double decimal = 2/5;
+		char character = '!';
+		String sentence = "sentence";
+		System.out.println("This is a number:");
+		System.out.println(decimal);
+	}
+	public static void main(String[] args) {
+		new Practice();
+
+	}
+
+}
